702605a50ff7f3e409792c738877259ceb9cacce
Sharon Manrique-Jimenez <sharon.manrique2@hotmail.com>
2019-05-18 17:36:43 -0400

    Font

--- src/Components/AppHeader/AppHeader.tsx
+++ src/Components/AppHeader/AppHeader.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import { Button, Icon, Input, Typography } from 'antd';
-import { Search } from 'history';
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { Button, Icon, Input, Typography } from "antd";
+import { Search } from "history";
+import "./AppHeader.css";
 
 function parseSearch(search: Search): string {
-  return new URLSearchParams(search).get('q') || '';
+  return new URLSearchParams(search).get("q") || "";
 }
 
 interface TitleBarProps {
@@ -12,19 +13,9 @@ interface TitleBarProps {
 }
 
 const TitleBar: React.FC<TitleBarProps> = props => (
-  <div style={{ display: 'flex', padding: 12 }}>
-    <Typography.Title
-      style={{
-        margin: 0,
-        padding: 0,
-        flexGrow: 1,
-        fontFamily: '"Julius Sans One", sans-serif',
-        fontSize: 42
-      }}
-      level={1}
-    >
-      ETHNYC TRACKER
-    </Typography.Title>
+  <div style={{ display: "flex", padding: 12 }}>
+    <div className="AppHeader-title">ETHNYC TRACKER</div>
+
     <div style={{ flexShrink: 0 }}>
       <a
         href="https://github.com/ethny-tracker/"
@@ -32,14 +23,14 @@ const TitleBar: React.FC<TitleBarProps> = props => (
         target="_blank"
       >
         <Button size="large" type="link" ghost onClick={props.onClick}>
-          <Icon type="github" style={{ fontSize: '50px' }}/>
+          <Icon type="github" style={{ fontSize: "50px" }} />
         </Button>
       </a>
     </div>
   </div>
 );
 
-const excludedRoutes = [ '/', '/upload' ];
+const excludedRoutes = ["/", "/upload"];
 
 const AppHeader = ({ location, history }: RouteComponentProps) => {
   if (excludedRoutes.indexOf(location.pathname) !== -1) {
@@ -47,9 +38,9 @@ const AppHeader = ({ location, history }: RouteComponentProps) => {
   }
 
   return (
-    <header style={{ padding: 20, display: 'flex', justifyContent: 'center' }}>
-      <div style={{ maxWidth: 800, width: '100%' }}>
-        <TitleBar onClick={() => history.push('/')}/>
+    <header style={{ padding: 20, display: "flex", justifyContent: "center" }}>
+      <div style={{ maxWidth: 800, width: "100%" }}>
+        <TitleBar onClick={() => history.push("/")} />
         <div>
           <Input.Search
             placeholder="Search for a file..."
