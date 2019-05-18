c41807b1ee246c2647e78d2a3282e4f5eb120c7e
Will O'Beirne <wbobeirne@gmail.com>
2019-05-18 12:53:58 -0400

    Functional mock db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,12 @@
 import Dexie from 'dexie';
-import { number } from 'prop-types';
+import { generateInode } from './mock';
 
 export interface Inode {
   id: string;
   title: string;
   description: string;
   category: string;
+  createdAt: number;
   mimeType: string;
   sizeBytes: number;
   author: string;
@@ -27,16 +28,18 @@ export type SyncUpdate = {
   inode: Inode;
 } & SyncState;
 
-export type SyncUpdateCallback =
-  | ((err: Error, data: undefined) => void)
-  | ((err: undefined, data: SyncUpdate) => void);
+export type SyncUpdateSuccessCallback = (err: undefined, data: SyncUpdate) => void;
+export type SyncUpdateErrorCallback = (err: Error, data: undefined) => void;
+export type SyncUpdateCallback = SyncUpdateSuccessCallback | SyncUpdateErrorCallback;
 
-export class InodeDatabase {
-  db: Dexie;
+const TOTAL = 100;
 
-  constructor(contractAddr: string) {
-    this.db = new Dexie(`inodes-${contractAddr}`);
-    this.db.version(1).stores({
+class InodeDexie extends Dexie {
+  inodes: Dexie.Table<Inode, string>;
+
+  constructor(name: string) {
+    super(name);
+    this.version(1).stores({
       inodes: [
         'id',
         'title',
@@ -48,39 +51,77 @@ export class InodeDatabase {
         'dataUri',
       ].join(','),
     });
+    this.inodes = this.table('inodes');
+  }
+}
+
+export class InodeDatabase {
+  private contractAddress: string;
+  private db: InodeDexie;
+
+  constructor(contractAddress: string) {
+    this.contractAddress = contractAddress;
+    this.db = new InodeDexie(`inodes-${contractAddress}`);
   }
 
-  startSync(cb: SyncUpdateCallback) {
-    return;
+  async startSync(cb: SyncUpdateCallback) {
+    const numSynced = await this.db.inodes.count();
+    for (let i = numSynced; i < TOTAL; i++) {
+      try {
+        const inode = await generateInode();
+        await this.db.inodes.add(inode);
+        (cb as SyncUpdateSuccessCallback)(undefined, {
+          inode,
+          total: TOTAL,
+          numSynced: i + 1,
+        });
+      } catch(err) {
+        (cb as SyncUpdateErrorCallback)(err as Error, undefined);
+      }
+    }
   }
 
-  getSyncState(): Promise<SyncState> {
-    return Promise.resolve({
-      numSynced: 0,
-      total: 0,
-    });
+  async getSyncState(): Promise<SyncState> {
+    const numSynced = await this.db.inodes.count();
+    return {
+      numSynced,
+      total: TOTAL,
+    };
   }
 
-  search(
+  async clearData(): Promise<void> {
+    await this.db.delete();
+    this.db = new InodeDexie(`inodes-${this.contractAddress}`);
+  }
+
+  async search(
     query: string,
     limit: number = 10,
     offset: number = 0,
   ): Promise<Pageable<Inode>> {
-    return Promise.resolve({
-      data: [],
-      total: 0,
-      end: true,
-    });
+    const inodes = this.db.inodes.filter(inode => inode.title.includes(query));
+    const total = await inodes.count();
+    const data = await inodes.offset(offset).limit(limit).toArray();
+    const end = offset + data.length === total;
+    return {
+      data,
+      total,
+      end,
+    };
   }
 
-  latest(
+  async latest(
     limit: number = 10,
     offset: number = 0,
   ): Promise<Pageable<Inode>> {
-    return Promise.resolve({
-      data: [],
-      total: 0,
-      end: true,
-    });
+    const inodes = this.db.inodes.orderBy('createdAt');
+    const total = await inodes.count();
+    const data = await inodes.offset(offset).limit(limit).toArray();
+    const end = offset + data.length === total;
+    return {
+      data,
+      total,
+      end,
+    };
   }
 }
