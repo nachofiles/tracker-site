cfb36933e23045f500511ff91b1dc95b9532ac9f
henrynguyen5 <ngu.henry@hotmail.com>
2019-05-18 13:33:31 -0400

    Add more store methods

--- src/store/InodeStore.ts
+++ src/store/InodeStore.ts
@@ -6,13 +6,15 @@ interface SearchParams {
   page: number;
 }
 
-interface GetLatestParams {}
+interface GetLatestParams {
+  page: number;
+}
 
 export class InodeStore {
   // results of the search request
   public searchResults: Inode[] = [];
   // total number of pages from the search result
-  public pages: number = 0;
+  public pages: number = 1;
   // the number of results to show per page
   public resultsPerPage: number;
 
@@ -35,9 +37,22 @@ export class InodeStore {
 
     const searchResults = await this.db.search(params.query, limit, offset);
 
-    this.pages = searchResults.total / this.resultsPerPage;
+    this.pages = Math.ceil(searchResults.total / this.resultsPerPage);
     this.searchResults = searchResults.data;
   }
 
-  public async getLatest() {}
+  public async getLatest(params: GetLatestParams) {
+    const limit = this.resultsPerPage;
+    const offset = this.resultsPerPage * params.page;
+
+    const searchResults = await this.db.latest(limit, offset);
+
+    this.pages = Math.ceil(searchResults.total / this.resultsPerPage);
+    this.searchResults = searchResults.data;
+  }
+
+  public async clear() {
+    await this.db.clearData();
+    this.searchResults = [];
+  }
 }
--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -2,13 +2,13 @@ import { configure } from "mobx";
 import { InodeStore } from "./InodeStore";
 
 export class RootStore {
-  public readonly inode: InodeStore;
+  //   public readonly inode: InodeStore;
 
   constructor() {
     configure({
       enforceActions: true
     });
 
-    this.inode = new InodeStore(this);
+    // this.inode = new InodeStore(this);
   }
 }
