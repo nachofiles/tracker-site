0a83c0d6a7bd79bfda16ec8bc547bdb9cd949306
henrynguyen5 <ngu.henry@hotmail.com>
2019-05-19 00:51:33 -0400

    Work on download props

--- src/Components/Download/Download.tsx
+++ src/Components/Download/Download.tsx
@@ -1,55 +1,61 @@
 import { Typography, Button, Tag } from "antd";
 import React from "react";
 import "./Download.css";
+import { RouteComponentProps } from "react-router";
 
 const { Title } = Typography;
 
-export function Download() {
-  return (
-    <div className="Download-container">
-      <div className="Download-content">
-        <div className="Download-header">
-          <div className="Download-title">
-            <Title level={2}>Education PDF</Title>
-          </div>
-          <div className="Download-title-specs">
-            <div className="Download-header-created">
-              Created: May, 28, 2019
-              <div className="Download-author">ANON</div>
+interface Props extends RouteComponentProps<{ id: string }> {
+  title: string;
+  created: string;
+  author: string;
+  description: string;
+
+  fileType: string;
+  fileSize: string;
+  id: string;
+  category: string;
+}
+
+export class Download extends React.Component<Props> {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+  }
+
+  public render() {
+    return (
+      <div className="Download-container">
+        <div className="Download-content">
+          <div className="Download-header">
+            <div className="Download-title">
+              <Title level={2}>{this.props.title}</Title>
+            </div>
+            <div className="Download-title-specs">
+              <div className="Download-header-created">
+                Created: {this.props.created}
+                <div className="Download-author">{this.props.author}</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="Download-body">
-          {" "}
-          <div className="Download-description">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy.{" "}
-          </div>
-          <div className="Download-footer">
+          <div className="Download-body">
             {" "}
-            <Tag color="volcano">Education</Tag>
-            <Tag color="magenta">PDF</Tag>{" "}
-            <Tag color="green">FILESIZE: 32gb</Tag>
-            <Tag color="purple">
+            <div className="Download-description">{this.props.description}</div>
+            <div className="Download-footer">
               {" "}
-              ID:
-              qETKm0D$73%1&jf6vPvaM9iMV9jPIpvnq&ii^F(YP1au4DjNpRmuCtbIl6jvwrVu
-            </Tag>
+              <Tag color="volcano">{this.props.category}</Tag>
+              <Tag color="magenta">{this.props.fileType}</Tag>{" "}
+              <Tag color="green">{this.props.fileSize}</Tag>
+              <Tag color="purple"> ID: {this.props.id}</Tag>
+            </div>
           </div>
-        </div>
-        <div className="Download-button">
-          {" "}
-          <Button type="primary" shape="round" icon="download" block>
-            Download
-          </Button>
+          <div className="Download-button">
+            {" "}
+            <Button type="primary" shape="round" icon="download" block>
+              Download
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
