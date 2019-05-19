fa09a72dd9bd02c5e75437d4c5e1d36d2f50da3e
Sharon Manrique-Jimenez <sharon.manrique2@hotmail.com>
2019-05-18 20:02:59 -0400

    upload styles

--- src/Components/Upload/Upload.tsx
+++ src/Components/Upload/Upload.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import { Button, Form, Input, Typography } from 'antd';
-import { inject, observer } from 'mobx-react';
-import { RootStore } from '../../store/rootStore';
+import React from "react";
+import { Button, Form, Input } from "antd";
+import { inject, observer } from "mobx-react";
+import { RootStore } from "../../store/rootStore";
+import "./Upload.css";
 
 interface UploadFormValue {
   title: string;
+  date: string;
+  author: string;
   description: string;
   category: string;
+  fileType: string;
+  fileSize: number;
+  id: string;
   ipfsHash: string;
 }
 
 const defaultValue: UploadFormValue = {
-  title: '',
-  description: '',
-  category: '',
-  ipfsHash: ''
+  title: "",
+  date: "",
+  author: "",
+  description: "",
+  category: "",
+  fileSize: 0,
+  fileType: "",
+  id: "",
+  ipfsHash: ""
 };
 
-const UploadForm = ({ disabled, value, onChange }: { disabled: boolean, value: UploadFormValue, onChange: (value: UploadFormValue) => void }) => {
+const UploadForm = ({
+  disabled,
+  value,
+  onChange
+}: {
+  disabled: boolean;
+  value: UploadFormValue;
+  onChange: (value: UploadFormValue) => void;
+}) => {
   return (
     <Form>
       <Form.Item label="Title">
@@ -25,55 +44,60 @@ const UploadForm = ({ disabled, value, onChange }: { disabled: boolean, value: U
           value={value.title}
           disabled={disabled}
           onChange={e => onChange({ ...value, title: e.target.value })}
-          placeholder="Title"/>
+          placeholder="Title"
+        />
       </Form.Item>
       <Form.Item label="Description">
         <Input.TextArea
           value={value.description}
           disabled={disabled}
           onChange={e => onChange({ ...value, description: e.target.value })}
-          placeholder="Description"/>
+          placeholder="Description"
+        />
       </Form.Item>
-
-      <Form.Item label="Category">
-        <Input
-          disabled={disabled}
-          value={value.category}
-          onChange={e => onChange({ ...value, category: e.target.value })}
-          placeholder="Category"/>
-      </Form.Item>
-
       <Form.Item label="IPFS hash">
         <Input
           disabled={disabled}
           value={value.ipfsHash}
           onChange={e => onChange({ ...value, ipfsHash: e.target.value })}
-          placeholder="IPFS Hash"/>
+          placeholder="IPFS Hash"
+        />
       </Form.Item>
     </Form>
   );
 };
 
-
-@inject('store')
+@inject("store")
 @observer
-export class Upload extends React.Component<{ store: RootStore }, { uploadFormValue: UploadFormValue }> {
+export class Upload extends React.Component<
+  { store: RootStore },
+  { uploadFormValue: UploadFormValue }
+> {
   state = {
     uploadFormValue: defaultValue
   };
 
-  handleChange = (uploadFormValue: UploadFormValue) => {
+  private handleChange = (uploadFormValue: UploadFormValue) => {
     this.setState({ uploadFormValue });
   };
 
+  private handleUpload = () => {
+    console.log("i was clicked");
+  };
+
   upload = () => {
-    const { title, description, category, ipfsHash } = this.state.uploadFormValue;
+    const {
+      title,
+      description,
+      category,
+      ipfsHash
+    } = this.state.uploadFormValue;
 
     this.props.store.inode.createInode({
       title,
       description,
       category,
-      uri: 'ipfs://' + ipfsHash
+      uri: "ipfs://" + ipfsHash
     });
   };
 
@@ -82,18 +106,28 @@ export class Upload extends React.Component<{ store: RootStore }, { uploadFormVa
     const { uploadFormValue } = this.state;
 
     return (
-      <div style={{ padding: 20 }}>
-        <Typography.Title level={2}>Upload a file</Typography.Title>
-        <UploadForm
-          disabled={creating}
-          value={uploadFormValue}
-          onChange={this.handleChange}/>
-        {createError !== null ? <div>{createError.message}</div> : null}
-        <Button
-          loading={creating}
-          onClick={this.upload}>Upload</Button>
+      <div className="Upload-container">
+        <div className="Upload-title"> Upload Your File</div>
+        <div className="Upload-form-container">
+          <div className="Upload-form">
+            <UploadForm
+              disabled={creating}
+              value={uploadFormValue}
+              onChange={this.handleChange}
+            />
+            {createError !== null ? <div>{createError.message}</div> : null}
+            <Button
+              type="primary"
+              shape="round"
+              icon="upload"
+              block
+              onClick={this.handleUpload}
+            >
+              Upload Your File
+            </Button>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
