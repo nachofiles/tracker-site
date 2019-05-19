d8a85732c1dd4b34f48071c2f4c63a0003b5c3fb
Moody Salem <moody.salem@gmail.com>
2019-05-18 21:33:18 -0400

    fix the styling in the title bar

--- src/Components/AppHeader/AppHeader.tsx
+++ src/Components/AppHeader/AppHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { Button, Icon, Input, Typography } from 'antd';
+import { Button, Icon, Input } from 'antd';
 import { Search } from 'history';
 
 function parseSearch(search: Search): string {
@@ -13,18 +13,16 @@ interface TitleBarProps {
 
 const TitleBar: React.FC<TitleBarProps> = props => (
   <div style={{ display: 'flex', padding: 12 }}>
-    <Typography.Title
-      style={{
-        margin: 0,
-        padding: 0,
-        flexGrow: 1,
-        fontFamily: '"Julius Sans One", sans-serif',
-        fontSize: 42
-      }}
-      level={1}
-    >
+    <div style={{
+      margin: 0,
+      padding: 0,
+      flexGrow: 1,
+      fontFamily: '"Julius Sans One", sans-serif',
+      fontSize: 42,
+      cursor: 'pointer'
+    }}>
       ETHNYC TRACKER
-    </Typography.Title>
+    </div>
     <div style={{ flexShrink: 0 }}>
       <a
         href="https://github.com/ethny-tracker/"
