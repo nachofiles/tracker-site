ef71a8c1f6013d8c79fab62185444ed48d680a76
Will O'Beirne <wbobeirne@gmail.com>
2019-05-19 02:59:44 -0400

    Sync state

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,34 +1,70 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
-import { Input } from "antd";
+import { Input, Popconfirm, Icon } from "antd";
 import { TitleBar } from "../AppHeader/AppHeader";
 import { Link } from "react-router-dom";
+import { inject, observer } from 'mobx-react';
+import { RootStore } from '../../store/rootStore';
 import "./HomePage.css";
 
-const HomePage = (props: RouteComponentProps) => {
-  return (
-    <div className="HomePage-container">
-      <div style={{ width: "100%", maxWidth: 800, padding: 20 }}>
-        <div>
-          <TitleBar onClick={() => props.history.push("/")} />
+interface Props extends RouteComponentProps {
+  store: RootStore;
+}
+
+@inject('store')
+@observer
+class HomePage extends React.Component<Props> {
+  private clearDb = async () => {
+    await this.props.store.inode.clear();
+    window.location.reload();
+  };
+
+  render() {
+    const { inode } = this.props.store;
+
+    let syncText;
+    if (inode.totalInodesToSync === Infinity) {
+      syncText = '';
+    } else if (inode.inodesSynced === inode.totalInodesToSync) {
+      syncText = 'Fully synced!';
+    } else {
+      syncText = `Synced ${inode.inodesSynced} of ${inode.totalInodesToSync}`;
+    }
+
+    return (
+      <div className="HomePage-container">
+        <div style={{ width: "100%", maxWidth: 800, padding: 20 }}>
+          <div>
+            <TitleBar onClick={() => this.props.history.push("/")} />
+          </div>
+          <Input.Search
+            className="HomePage-search"
+            size="large"
+            placeholder="Search for a file..."
+            onSearch={e => this.props.history.push(`/search?q=${e}`)}
+          />
+          <div className="HomePage-details">
+            <ul className="link-list">
+              <li>
+                <Link to="/search?q=">Browse files</Link>
+              </li>
+              <li>
+                <Link to="/upload">Upload file</Link>
+              </li>
+              <li>
+                <Popconfirm title="Clear & redownload file database?" onConfirm={this.clearDb}>
+                  <a>Clear database</a>
+                </Popconfirm>
+              </li>
+            </ul>
+            <div className={`HomePage-sync ${syncText ? '' : 'is-hidden'}`}>
+              {syncText}
+            </div>
+          </div>
         </div>
-        <Input.Search
-          className="HomePage-search"
-          size="large"
-          placeholder="Search for a file..."
-          onSearch={e => props.history.push(`/search?q=${e}`)}
-        />
-        <ul className="link-list">
-          <li>
-            <Link to="/search?q=">Browse files</Link>
-          </li>
-          <li>
-            <Link to="/upload">Upload file</Link>
-          </li>
-        </ul>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default HomePage;
